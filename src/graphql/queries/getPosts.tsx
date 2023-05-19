96d59c90e25efecce91c76281d964d9eb75320c9
jhs1391 <jhs1391@gmail.com>
2023-05-19 12:40:56 -0500

    no message

---
+++ src/graphql/queries/getPosts.tsx
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const GET_POSTS = gql`
+  query GetPosts {
+    posts {
+      nodes {
+        id
+        title
+        content
+      }
+    }
+  }
+`;
